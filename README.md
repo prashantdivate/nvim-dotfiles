6aa1c107546469c757ccb76a397e82e0eb09014e
Prashant Divate <diwateprashant44@gmail.com>
2024-07-26 18:44:02 +0530

    My dotvim configs

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# nvim-dotfiles
+# nvim-dotfiles for linux
+
+
+##Step 1 : Clone th project 
+
+```
+git clone https://github.com/prashantdivate/nvim-dotfiles.git ~/.config/nvim/
+```
+
+##Step 2 : Install nodejs and python3-venv
+```
+sudo apt install node python3.10-venv
+```
---
+++ lua/core/plugin_config/colorscheme.lua
@@ -0,0 +1,12 @@
+require("catppuccin").setup({
+  flavour = "mocha",
+  transparent_background = true,
+  styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+    comments = { "italic" }, -- Change the style of comments
+  },
+})
+
+vim.o.termguicolors = true
+vim.o.background = "dark"
+--vim.cmd[[colorscheme catppuccin]]
+vim.cmd[[colorscheme gruvbox]]
